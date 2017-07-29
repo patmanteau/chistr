c46eff1967f1e0e9d1d0ad8502f16188c0806392
Patrick Haas <patmanteau@posteo.de>
2017-07-29 12:41:59 +0200

    Remember sorting

--- src/renderer/store/modules/Interface.js
+++ src/renderer/store/modules/Interface.js
@@ -1,23 +1,24 @@
 import * as types from '../mutation-types'
+import config from '../../../config'
 
 const state = {
-  playerListSort: {
-    key: 'playerWinrate',
-    order: 1
-  }
+  playerListSort: config.get('app.playerListSort')
 }
+
+console.log(state)
 
 const getters = {}
 
 const mutations = {
   [types.SET_PLAYER_LIST_SORT] (state, obj) {
     Object.assign(state.playerListSort, obj)
+    config.set('app.playerListSort', obj)
   }
 }
 
 const actions = {
   setPlayerListSortKey ({ state, commit }, key) {
-    let newSort = {}
+    let newSort = { ...state.playerListSort }
     if (state.playerListSort.key === key) {
       newSort.order = state.playerListSort.order * -1
     }
